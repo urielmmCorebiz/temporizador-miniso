46cd3545215cf5bc5adb17ecb21bcf4153a469c6
MatheusAraujo42 <matheus.araujo@corebiz.com.br>
2020-03-26 14:55:55 -0300

    feat: add message builder

--- react/HelloWorld.js
+++ react/HelloWorld.js
@@ -1,6 +1,13 @@
 import React from 'react'
+import { injectIntl } from 'react-intl'
 
-const HelloWorld = () => (<div>Hello World</div>)
+const HelloWorld = ({
+  intl
+}) => {
+  const message = intl.formatMessage({ id: 'store/sandbox.helloworld' });
+
+  return <div>{message}</div>;
+}
 
 
-export default HelloWorld;
+export default injectIntl(HelloWorld);
