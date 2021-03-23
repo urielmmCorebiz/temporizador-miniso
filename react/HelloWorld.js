694e3589689a842f74e4d09435f458af0ab8cd91
UrieelDev <uriel.mejia@corebiz.ag>
2021-03-23 10:52:03 -0600

    first commit

--- react/HelloWorld.js
+++ react/HelloWorld.js
@@ -1,10 +1,42 @@
 import React from "react";
+import Countdown from 'react-countdown';
+import "./HelloWorld.css";
 
 const HelloWorld = () => {
 
+  //El mensaje que mostrará al terminar el temporizador
+  const Completionist = () => <div className="temp-hotsale">
+              <img src="https://www.hotsale.com.co/public/imgs/hot-sale-2021.svg" alt="Hot Sale Icon" />
+
+    <p className="temp-hotsale__text">¡El HotSale Miniso ha comenzado!</p></div>;
+
+  const renderer = ({days, hours, minutes, seconds, completed }) => {
+    if (completed) {
+      return <div className="temp-hotsale"> 
+        <Completionist className="temp-hotsale__text" />
+      </div> 
+    } else {
+      return <div className="container-temp" >
+        <div className="container-temp__text"   >
+         <div className="container-temp__responsive">
+         <img src="https://www.hotsale.com.co/public/imgs/hot-sale-2021.svg" alt="Hot Sale Icon"/>
+        <p >El HotSale Miniso inicia en:</p>
+         </div>
+        <span>{days} Días {hours} Horas {minutes} Minutos {seconds} Segundos</span>
+        </div>
+      </div> 
+    }
+  };
+  
   return (
-    <div>HOLA MUNDO</div> 
-   )
+    //Establecer la fecha límite del temporizador
+    <div>
+    <Countdown
+    date={"2021-03-24T00:00:00"}
+    renderer={renderer}
+  />
+    </div>
+  )
   }
 
 export default HelloWorld;
